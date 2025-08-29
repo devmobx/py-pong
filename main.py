c0289fd17761fc2760d410fe46603187e8a1e0db
Craig Chick <craig.chick.ext@up.coop>
2025-08-29 22:06:06 +0200

    moved game loop code into py_pong files

--- main.py
+++ main.py
@@ -1,36 +1,5 @@
-import pygame
-from sprites import Paddle, Ball, Score
-from constants import Config
-
-
-def initPyPong():
-    pygame.init()
-    window = pygame.display.set_mode((Config.WINDOW_WIDTH, Config.WINDOW_HEIGHT))
-    clock = pygame.time.Clock()
-    running = True
-
-    player_1 = Paddle(window, 1)
-    player_2 = Paddle(window, 2)
-    ball = Ball(window, player_1, player_2)
-    score = Score(window, player_1, player_2, ball)
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        window.fill((0, 0, 0))
-
-        for sprite in [player_1, player_2, ball, score]:
-            sprite.handler()
-            sprite.draw()
-
-        pygame.display.flip()
-
-        clock.tick(60)
-
-    pygame.quit()
+from py_pong import PyPong
 
 
 if __name__ == "__main__":
-    initPyPong()
+    PyPong.init()
---
+++ py_pong.py
@@ -0,0 +1,33 @@
+import pygame
+from constants.config import Config
+from sprites import Ball, Paddle, Score
+
+
+class PyPong:
+    def init():
+        pygame.init()
+        window = pygame.display.set_mode((Config.WINDOW_WIDTH, Config.WINDOW_HEIGHT))
+        clock = pygame.time.Clock()
+        running = True
+
+        player_1 = Paddle(window, 1)
+        player_2 = Paddle(window, 2)
+        ball = Ball(window, player_1, player_2)
+        score = Score(window, player_1, player_2, ball)
+
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            window.fill((0, 0, 0))
+
+            for sprite in [player_1, player_2, ball, score]:
+                sprite.handler()
+                sprite.draw()
+
+            pygame.display.flip()
+
+            clock.tick(60)
+
+        pygame.quit()
